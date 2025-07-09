1f5c093212ee4f3088c44e306fa2bee05591d79b
FutuXer <59602001@qq.com>
2025-07-09 20:36:44 +0800

    Finalize syscall lab changes before switching to pgtbl

--- kernel/sysinfo.h
+++ kernel/sysinfo.h
@@ -1,13 +1,4 @@
-<<<<<<< HEAD
 struct sysinfo {
   uint64 freemem;   // amount of free memory (bytes)
   uint64 nproc;     // number of process
 };
-=======
-
-struct sysinfo 
-{
-  uint64 freemem;   // 可用内存字节数
-  uint64 nproc;     // 非 UNUSED 状态的进程数
-};
->>>>>>> e540eec (Save my current syscall changes before switching to lab branch)
