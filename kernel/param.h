4c7962a9e2b43a9d26664eccd6f81a86f194ab04
FutuXer <59602001@qq.com>
2025-07-17 19:51:18 +0800

    完成 Lab10: mmap

--- kernel/param.h
+++ kernel/param.h
@@ -11,3 +11,15 @@
 #define NBUF         (MAXOPBLOCKS*3)  // size of disk block cache
 #define FSSIZE       1000  // size of file system in blocks
 #define MAXPATH      128   // maximum file path name
+
+// Protection flags
+#define PROT_NONE       0x0     /* no permissions */
+#define PROT_READ       0x1     /* pages can be read */
+#define PROT_WRITE      0x2     /* pages can be written */
+#define PROT_EXEC       0x4     /* pages can be executed */ // <-- 添加这一行
+
+// Flags for mmap
+#define MAP_SHARED      0x01    /* Share changes */
+#define MAP_PRIVATE     0x02    /* Changes are private */
+#define MAP_ANONYMOUS   0x04    /* Don't use a file */
+#define MAP_FIXED       0x10    /* Interpret addr exactly */
